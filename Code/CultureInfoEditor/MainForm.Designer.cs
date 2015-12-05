59da61aac534c3ac05352072b93790218bf4ac28
David Parvin <dwparvin@cableone.net>
2015-12-04 23:10:55 -0700

    Form started basic design

--- Code/CultureInfoEditor/MainForm.Designer.cs
+++ Code/CultureInfoEditor/MainForm.Designer.cs
@@ -28,20 +28,323 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.layout = new System.Windows.Forms.TableLayoutPanel();
+            this.tabs = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.GeneralProperties = new System.Windows.Forms.PropertyGrid();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.NumberProperties = new System.Windows.Forms.PropertyGrid();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.CurrencyProperties = new System.Windows.Forms.PropertyGrid();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.TimeProperties = new System.Windows.Forms.PropertyGrid();
+            this.tabPage5 = new System.Windows.Forms.TabPage();
+            this.DateProperties = new System.Windows.Forms.PropertyGrid();
+            this.tabPage6 = new System.Windows.Forms.TabPage();
+            this.CalendarProperties = new System.Windows.Forms.PropertyGrid();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.mainMenu = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.registerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.unregisterToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.layout.SuspendLayout();
+            this.tabs.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            this.tabPage4.SuspendLayout();
+            this.tabPage5.SuspendLayout();
+            this.tabPage6.SuspendLayout();
+            this.mainMenu.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // layout
+            // 
+            this.layout.ColumnCount = 4;
+            this.layout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.layout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.layout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.layout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.layout.Controls.Add(this.tabs, 1, 2);
+            this.layout.Controls.Add(this.label1, 1, 1);
+            this.layout.Controls.Add(this.textBox1, 2, 1);
+            this.layout.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.layout.Location = new System.Drawing.Point(0, 24);
+            this.layout.Name = "layout";
+            this.layout.RowCount = 4;
+            this.layout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 10F));
+            this.layout.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.layout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.layout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.layout.Size = new System.Drawing.Size(686, 461);
+            this.layout.TabIndex = 0;
+            // 
+            // tabs
+            // 
+            this.layout.SetColumnSpan(this.tabs, 2);
+            this.tabs.Controls.Add(this.tabPage1);
+            this.tabs.Controls.Add(this.tabPage2);
+            this.tabs.Controls.Add(this.tabPage3);
+            this.tabs.Controls.Add(this.tabPage4);
+            this.tabs.Controls.Add(this.tabPage5);
+            this.tabs.Controls.Add(this.tabPage6);
+            this.tabs.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabs.Location = new System.Drawing.Point(23, 39);
+            this.tabs.Name = "tabs";
+            this.tabs.SelectedIndex = 0;
+            this.tabs.Size = new System.Drawing.Size(640, 399);
+            this.tabs.TabIndex = 2;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.GeneralProperties);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(632, 373);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "General";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // GeneralProperties
+            // 
+            this.GeneralProperties.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.GeneralProperties.Location = new System.Drawing.Point(3, 3);
+            this.GeneralProperties.Name = "GeneralProperties";
+            this.GeneralProperties.Size = new System.Drawing.Size(626, 367);
+            this.GeneralProperties.TabIndex = 0;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.NumberProperties);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(632, 373);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Number";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // NumberProperties
+            // 
+            this.NumberProperties.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.NumberProperties.Location = new System.Drawing.Point(3, 3);
+            this.NumberProperties.Name = "NumberProperties";
+            this.NumberProperties.Size = new System.Drawing.Size(626, 367);
+            this.NumberProperties.TabIndex = 0;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.CurrencyProperties);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Size = new System.Drawing.Size(632, 373);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Currency";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // CurrencyProperties
+            // 
+            this.CurrencyProperties.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.CurrencyProperties.Location = new System.Drawing.Point(0, 0);
+            this.CurrencyProperties.Name = "CurrencyProperties";
+            this.CurrencyProperties.Size = new System.Drawing.Size(632, 373);
+            this.CurrencyProperties.TabIndex = 0;
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Controls.Add(this.TimeProperties);
+            this.tabPage4.Location = new System.Drawing.Point(4, 22);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Size = new System.Drawing.Size(632, 373);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "Time";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // TimeProperties
+            // 
+            this.TimeProperties.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.TimeProperties.Location = new System.Drawing.Point(0, 0);
+            this.TimeProperties.Name = "TimeProperties";
+            this.TimeProperties.Size = new System.Drawing.Size(632, 373);
+            this.TimeProperties.TabIndex = 0;
+            // 
+            // tabPage5
+            // 
+            this.tabPage5.Controls.Add(this.DateProperties);
+            this.tabPage5.Location = new System.Drawing.Point(4, 22);
+            this.tabPage5.Name = "tabPage5";
+            this.tabPage5.Size = new System.Drawing.Size(632, 373);
+            this.tabPage5.TabIndex = 4;
+            this.tabPage5.Text = "Date";
+            this.tabPage5.UseVisualStyleBackColor = true;
+            // 
+            // DateProperties
+            // 
+            this.DateProperties.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.DateProperties.Location = new System.Drawing.Point(0, 0);
+            this.DateProperties.Name = "DateProperties";
+            this.DateProperties.Size = new System.Drawing.Size(632, 373);
+            this.DateProperties.TabIndex = 0;
+            // 
+            // tabPage6
+            // 
+            this.tabPage6.Controls.Add(this.CalendarProperties);
+            this.tabPage6.Location = new System.Drawing.Point(4, 22);
+            this.tabPage6.Name = "tabPage6";
+            this.tabPage6.Size = new System.Drawing.Size(632, 373);
+            this.tabPage6.TabIndex = 5;
+            this.tabPage6.Text = "Calendar";
+            this.tabPage6.UseVisualStyleBackColor = true;
+            // 
+            // CalendarProperties
+            // 
+            this.CalendarProperties.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.CalendarProperties.Location = new System.Drawing.Point(0, 0);
+            this.CalendarProperties.Name = "CalendarProperties";
+            this.CalendarProperties.Size = new System.Drawing.Size(632, 373);
+            this.CalendarProperties.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.label1.Location = new System.Drawing.Point(23, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Culture Name";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.Location = new System.Drawing.Point(100, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(563, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // mainMenu
+            // 
+            this.mainMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.registerToolStripMenuItem,
+            this.unregisterToolStripMenuItem});
+            this.mainMenu.Location = new System.Drawing.Point(0, 0);
+            this.mainMenu.Name = "mainMenu";
+            this.mainMenu.Size = new System.Drawing.Size(686, 24);
+            this.mainMenu.TabIndex = 7;
+            this.mainMenu.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.saveToolStripMenuItem,
+            this.loadToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // loadToolStripMenuItem
+            // 
+            this.loadToolStripMenuItem.Name = "loadToolStripMenuItem";
+            this.loadToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.loadToolStripMenuItem.Text = "Load";
+            this.loadToolStripMenuItem.Click += new System.EventHandler(this.loadToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // registerToolStripMenuItem
+            // 
+            this.registerToolStripMenuItem.Name = "registerToolStripMenuItem";
+            this.registerToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.registerToolStripMenuItem.Text = "Register";
+            this.registerToolStripMenuItem.Click += new System.EventHandler(this.registerToolStripMenuItem_Click);
+            // 
+            // unregisterToolStripMenuItem
+            // 
+            this.unregisterToolStripMenuItem.Name = "unregisterToolStripMenuItem";
+            this.unregisterToolStripMenuItem.Size = new System.Drawing.Size(73, 20);
+            this.unregisterToolStripMenuItem.Text = "Unregister";
+            this.unregisterToolStripMenuItem.Click += new System.EventHandler(this.unregisterToolStripMenuItem_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(497, 389);
+            this.ClientSize = new System.Drawing.Size(686, 485);
+            this.Controls.Add(this.layout);
+            this.Controls.Add(this.mainMenu);
+            this.MainMenuStrip = this.mainMenu;
             this.Name = "MainForm";
             this.Text = "CultureInfoEditor";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.layout.ResumeLayout(false);
+            this.layout.PerformLayout();
+            this.tabs.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage3.ResumeLayout(false);
+            this.tabPage4.ResumeLayout(false);
+            this.tabPage5.ResumeLayout(false);
+            this.tabPage6.ResumeLayout(false);
+            this.mainMenu.ResumeLayout(false);
+            this.mainMenu.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TableLayoutPanel layout;
+        private System.Windows.Forms.TabControl tabs;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.PropertyGrid GeneralProperties;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.PropertyGrid NumberProperties;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.PropertyGrid CurrencyProperties;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.PropertyGrid TimeProperties;
+        private System.Windows.Forms.TabPage tabPage5;
+        private System.Windows.Forms.PropertyGrid DateProperties;
+        private System.Windows.Forms.TabPage tabPage6;
+        private System.Windows.Forms.PropertyGrid CalendarProperties;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.MenuStrip mainMenu;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem registerToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem unregisterToolStripMenuItem;
     }
 }
 
--- Code/CultureInfoEditor/MainForm.cs
+++ Code/CultureInfoEditor/MainForm.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Globalization;
 using System.Windows.Forms;
 
 namespace CultureInfoEditor
@@ -15,6 +8,37 @@ namespace CultureInfoEditor
         public MainForm()
         {
             InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, System.EventArgs e)
+        {
+            var obj = new CultureInfo("en-US");
+            GeneralProperties.SelectedObject = obj;
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, System.EventArgs e)
+        {
+
+        }
+
+        private void loadToolStripMenuItem_Click(object sender, System.EventArgs e)
+        {
+
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, System.EventArgs e)
+        {
+
+        }
+
+        private void registerToolStripMenuItem_Click(object sender, System.EventArgs e)
+        {
+
+        }
+
+        private void unregisterToolStripMenuItem_Click(object sender, System.EventArgs e)
+        {
+
         }
     }
 }
